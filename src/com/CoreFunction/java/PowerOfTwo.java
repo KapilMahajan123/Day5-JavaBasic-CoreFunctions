bb4c66446e4f6bee3aa233ab9ccccb6228342860
KapilMahajan123 <111445779+KapilMahajan123@users.noreply.github.com>
2022-11-24 16:19:01 +0530

    Added: to check Power of two values

--- src/com/CoreFunction/java/PowerOfTwo.java
+++ src/com/CoreFunction/java/PowerOfTwo.java
@@ -12,7 +12,7 @@ public class PowerOfTwo {
 	public static void main(String[] args) {
 		
 		// This program takes a command-line argument N and prints a table of the
-	    //powers of 2 that are less than or equal to 2^N.
+	       //powers of 2 that are less than or equal to 2^N.
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter the value for N : ");
 		int number = scanner.nextInt();
